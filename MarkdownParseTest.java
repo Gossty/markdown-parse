baf2c0a5eaffc01041a6f03618587bc85b6b6a52
Gosty <91396884+Gossty@users.noreply.github.com>
2022-02-04 02:41:29 +0300

    taking the mistake away(NotEquals)

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,7 +25,7 @@ public class MarkdownParseTest {
         Path fileName = Path.of("new-one.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertNotEquals("Test the links from the new-one.md", List.of("google.com"), links);
+        assertEquals("Test the links from the new-one.md", List.of("google.com"), links);
     }
     @Test
     public void newTwo() throws IOException {
